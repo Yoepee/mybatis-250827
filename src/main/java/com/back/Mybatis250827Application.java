fdb118461f58e3fcc35bcb853b0313d59cb7397e
Yoepee <kdy2975@naver.com>
2025-08-27 09:35:40 +0900

    init

---
+++ src/main/java/com/back/Mybatis250827Application.java
@@ -0,0 +1,13 @@
+package com.back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mybatis250827Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Mybatis250827Application.class, args);
+    }
+
+}
