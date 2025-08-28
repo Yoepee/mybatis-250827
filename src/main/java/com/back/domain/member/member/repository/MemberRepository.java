01b95fcbd1ce5fb528651d5abcc19a8ef2796f23
Yoepee <kdy2975@naver.com>
2025-08-28 14:30:00 +0900

    멤버 테스트 케이스 추가

--- src/main/java/com/back/domain/member/member/repository/MemberRepository.java
+++ src/main/java/com/back/domain/member/member/repository/MemberRepository.java
@@ -1,7 +1,20 @@
 package com.back.domain.member.member.repository;
 
+import com.back.domain.member.member.dto.Member;
 import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
 
 @Mapper
 public interface MemberRepository {
+    List<Member> findAll();
+
+    Member findById(int id);
+    Member findByUsername(String username);
+
+    int create(Member member);
+
+    void deleteById(int id);
+
+    int update(Member member);
 }
--- src/main/java/com/back/domain/member/member/service/MemberService.java
+++ src/main/java/com/back/domain/member/member/service/MemberService.java
@@ -1,11 +1,49 @@
 package com.back.domain.member.member.service;
 
+import com.back.domain.member.member.dto.Member;
 import com.back.domain.member.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @RequiredArgsConstructor
 @Service
 public class MemberService {
-    private MemberRepository memberRepository;
+    private final MemberRepository memberRepository;
+
+    public List<Member> findAll() {
+        return memberRepository.findAll();
+    }
+
+    public Member findById(int id) {
+        return memberRepository.findById(id);
+    }
+
+    public Member findByUsername(String username) {
+        return memberRepository.findByUsername(username);
+    }
+
+    public int create(String username, String password, String name, String email) {
+        if (findByUsername(username) != null) return -1;
+        Member member = new Member(username, password, name, email);
+        memberRepository.create(member);
+        return member.getId();
+    }
+
+    public void deleteById(int id) {
+        memberRepository.deleteById(id);
+    }
+
+    public void update(int id, String username, String password, String name, String email) {
+        Member member = findById(id);
+        member.setUsername(username);
+        member.setPassword(password);
+        member.setName(name);
+        member.setEmail(email);
+        int updatedRows = memberRepository.update(member);
+        if (updatedRows != 1){
+            throw new IllegalStateException("게시물 수정 실패");
+        }
+    }
 }
