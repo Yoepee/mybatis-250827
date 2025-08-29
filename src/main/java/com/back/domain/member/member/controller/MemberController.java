98ad304f27691df0d8d9b42d8dc521b99688d5ff
Yoepee <kdy2975@naver.com>
2025-08-29 15:35:51 +0900

    로그인 상태 확인

--- src/main/java/com/back/domain/member/member/controller/MemberController.java
+++ src/main/java/com/back/domain/member/member/controller/MemberController.java
@@ -1,10 +1,15 @@
 package com.back.domain.member.member.controller;
 
+import com.back.domain.member.member.dto.Member;
 import com.back.domain.member.member.service.MemberService;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequiredArgsConstructor
@@ -13,7 +18,26 @@ public class MemberController {
     private final MemberService memberService;
 
     @GetMapping("/login")
-    public String login() {
+    public String showLogin() {
         return "member/member/login";
+    }
+
+    @PostMapping("/login")
+    public String login(@Param("username") String username, @Param("password")String password, HttpSession session) {
+        Member member = memberService.findByUsername(username);
+        
+        // TODO: 존재하는 회원인지 검증
+        // TODO: 비밀번호 일치 검증
+        session.setAttribute("loginedMemberId", member.getId());
+        System.out.println(session.getAttribute("loginedMemberId"));
+        return "redirect:/posts/list";
+    }
+
+    @GetMapping("/logout")
+    @ResponseBody
+    public String logout(HttpSession session) {
+        session.removeAttribute("loginedMemberId");
+
+        return "success";
     }
 }
--- src/main/java/com/back/global/interceptor/NeedToLoginInterceptor.java
+++ src/main/java/com/back/global/interceptor/NeedToLoginInterceptor.java
@@ -2,6 +2,7 @@ package com.back.global.interceptor;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
@@ -13,7 +14,11 @@ public class NeedToLoginInterceptor implements HandlerInterceptor {
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         log.debug("NeedToLoginInterceptor::preHandle 실행됨");
 
-        boolean isLogined = false;
+        HttpSession session = request.getSession();
+        Integer loginedMemberId = (Integer) session.getAttribute("loginedMemberId");
+
+        // TODO: 검증 작업 추가 (현재 임시 처리)
+        boolean isLogined = loginedMemberId != null;
         if (!isLogined) {
             response.setCharacterEncoding("UTF-8");
             response.setContentType("text/html; charset=UTF-8");
