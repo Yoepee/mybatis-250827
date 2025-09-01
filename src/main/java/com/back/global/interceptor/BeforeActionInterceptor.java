69778d83e0c2d3c9de6d7514858cd5402322452b
Yoepee <kdy2975@naver.com>
2025-09-01 10:35:09 +0900

    싱글톤 빈 스코프

--- src/main/java/com/back/global/interceptor/BeforeActionInterceptor.java
+++ src/main/java/com/back/global/interceptor/BeforeActionInterceptor.java
@@ -1,19 +1,29 @@
 package com.back.global.interceptor;
 
+import com.back.global.Rq.Rq;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 @Component
 @Slf4j
+@RequiredArgsConstructor
 public class BeforeActionInterceptor implements HandlerInterceptor {
+    private final Rq rq;
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        log.debug("this"+this);
         log.debug("BeforeActionInterceptor::preHandle 실행됨");
         log.info("info");
         log.warn("warn");
+
+        rq.increaseCount();
+        log.debug("""
+                rq: %s, rq.count: %s
+                """.formatted(rq, rq.getCount()));
         return HandlerInterceptor.super.preHandle(request, response, handler);
     }
 }
